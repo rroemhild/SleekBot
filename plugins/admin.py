ee51fcaf420ee6261af1ebba3142f6cd40017e84
Hernan E Grecco <hernan-edgardo.grecco@mpi-dortmund.mpg.de>
2010-08-02 00:05:12 +0200

    Updated license information

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,20 +1,6 @@
 """
-    admin.py - A plugin for administering the bot.
-    Copyright (C) 2007 Kevin Smith
-
-    SleekBot is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    SleekBot is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this software; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    This file is part of SleekBot. http://github.com/hgrecco/SleekBot
+    See the README file for more information.
 """
 
 import logging
