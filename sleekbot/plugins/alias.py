79ac0a22c1a20bff5a7c35f452adcff176db0155
Rafael Römhild <rafael@roemhild.de>
2010-09-30 21:49:25 +0200

    Changed the alias plugin
    - set indexes on database table
    - make code more PEP8 compliant
    - split the acl command actions into several hidden commands
    - use the new parse_args method

--- sleekbot/plugins/alias.py
+++ sleekbot/plugins/alias.py
@@ -15,78 +15,92 @@ the user has precedence. -->
 </plugin>
 """
 
-import re
 import logging
 
 from sleekbot.commandbot import botcmd, botfreetxt
+from sleekbot.commandbot import parse_args, ArgError
 from sleekbot.plugbot import BotPlugin
 
 
-class aliascmd(object):
+class AliasCmd(object):
     """ Represent an aliased command
     """
 
-    def __init__(self, jid, alias, command=None):
-        """ Initialise aliascmd
-        """
-
+    def __init__(self, jid, aliascmd, command=None):
         self.jid = jid
-        self.alias = alias
+        self.alias = aliascmd
         self.command = command
 
 
-class aliasstore(object):
+class AliasStore(object):
+    """ Class for storing aliased commands into he database
+    """
+    
     def __init__(self, store):
         self.store = store
-        self.createTable()
+        self.create_table()
 
-    def createTable(self):
-        db = self.store.getDb()
-        if not len(db.execute("pragma table_info('alias')").fetchall()) > 0:
-            db.execute("""CREATE TABLE "alias" ("id" INTEGER PRIMARY KEY AUTOINCREMENT,
-                                       "jid" TEXT NOT NULL, "alias" TEXT NOT NULL,
-                                       "command" TEXT NOT NULL)""")
-        db.close()
+    def create_table(self):
+        """ Create the alias table.
+        """
+        with self.store.context_cursor() as cur:
+            if not len(cur.execute("pragma table_info('alias')").fetchall()) > 0:
+                cur.execute("""CREATE TABLE "alias" (
+                            "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+                            "jid" VARCHAR(256), "alias" VARCHAR(256),
+                            "command" VARCHAR(256))""")
+                cur.execute('CREATE INDEX idx_jid ON alias (jid)')
+                cur.execute('CREATE INDEX idx_jid_alias ON alias (jid, alias)')
+                logging.debug("alias table created")
 
-    def update(self, alias):
-        db = self.store.getDb()
-        cur = db.cursor()
-        cur.execute('SELECT * FROM alias WHERE jid=? AND alias=?', (alias.jid, alias.alias))
-        if (len(cur.fetchall()) > 0):
-            cur.execute('UPDATE alias SET jid=?, command=?, alias=? WHERE jid=? AND alias=?', (alias.jid, alias.command, alias.alias, alias.jid, alias.alias))
-        else:
-            cur.execute('INSERT INTO alias(jid, command, alias) VALUES(?,?,?)', (alias.jid, alias.command, alias.alias))
-        db.commit()
-        db.close()
+    def update(self, aliascmd):
+        """ Update or insert an aliased command.
+        """
+        with self.store.context_cursor() as cur:
+            cur.execute('SELECT * FROM alias WHERE jid=? AND alias=?',
+                    (aliascmd.jid, aliascmd.alias))
+            if (len(cur.fetchall()) > 0):
+                cur.execute('UPDATE alias SET jid=?, command=?, alias=?' \
+                    'WHERE jid=? AND alias=?', (aliascmd.jid, aliascmd.command,
+                                                aliascmd.alias, aliascmd.jid,
+                                                aliascmd.alias))
+            else:
+                cur.execute('INSERT INTO alias(jid, command, alias)' \
+                    'VALUES(?,?,?)', (aliascmd.jid, aliascmd.command,
+                    aliascmd.alias))
 
-    def get(self, alias):
-        db = self.store.getDb()
-        cur = db.cursor()
-        cur.execute('SELECT * FROM alias WHERE jid=? AND alias=?', (alias.jid, alias.alias))
-        results = cur.fetchall()
-        if len(results) == 0:
-            return None
-        return aliascmd(results[0][1], results[0][2], results[0][3])
-        db.close()
+    def get(self, aliascmd):
+        """ Get an alias.
+        """
+        with self.store.context_cursor() as cur:
+            cur.execute('SELECT * FROM alias WHERE jid=? AND alias=?',
+                    (aliascmd.jid, aliascmd.alias))
+            results = cur.fetchall()
+
+            if len(results) == 0:
+                return None
+            return AliasCmd(results[0][1], results[0][2], results[0][3])
 
     def get_all(self, jid):
-        db = self.store.getDb()
-        cur = db.cursor()
-        cur.execute('SELECT * FROM alias WHERE jid=?', (jid,))
-        results = cur.fetchall()
-        if len(results) == 0:
-            return None
-        response = []
-        for result in results:
-            response.append(aliascmd(result[1], result[2], result[3]))
-        return response
+        """ Get all aliases.
+        """
+        with self.store.context_cursor() as cur:
+            cur.execute('SELECT * FROM alias WHERE jid=?', (jid,))
+            results = cur.fetchall()
 
-    def delete(self, alias):
-        db = self.store.getDb()
-        cur = db.cursor()
-        cur.execute('DELETE FROM alias WHERE jid=? AND alias=?', (alias.jid, alias.alias))
-        db.commit()
-        db.close()
+            if len(results) == 0:
+                return None
+            response = []
+            for result in results:
+                response.append(AliasCmd(result[1], result[2], result[3]))
+            return response
+
+    def delete(self, aliascmd):
+        """ Delete an aliased command.
+        """
+        with self.store.context_cursor() as cur:
+            cur.execute('DELETE FROM alias WHERE jid=? AND alias=?',
+                    (aliascmd.jid, aliascmd.alias))
 
 
 class alias(BotPlugin):
@@ -96,31 +110,33 @@ class alias(BotPlugin):
     freetextRegex = ''
 
     def on_register(self):
+        """ On plugin load parse the freetextRegex together and set it global
+        so botfreetext can use it later.
+        """
         self.im_prefix = self.bot.im_prefix
         self.muc_prefix = self.bot.muc_prefix
-        self.aliasstore = aliasstore(self.bot.store)
+        self.alias_store = AliasStore(self.bot.store)
 
         # botfreetext regex string with im and mux prefix
         global freetextRegex
-        freetextRegex = "^[\%s\%s][a-zA-Z].*$" % (self.im_prefix,
-                                                  self.muc_prefix)
+        freetextRegex = "^[\%s\%s][a-zA-Z].*$" \
+                        % (self.im_prefix, self.muc_prefix)
 
         # global aliases
         self.global_aliases = {}
         if self.config:
-            for alias in self.config.findall('alias'):
-                logging.debug("Load global alias: %s" % alias.attrib['name'])
-                self.global_aliases[alias.attrib['name']] = aliascmd(None,
-                                                  alias.attrib['name'],
-                                                  alias.attrib['command'])
+            for aliascmd in self.config.findall('alias'):
+                logging.debug("Load global alias: %s" % aliascmd.attrib['name'])
+                self.global_aliases[aliascmd.attrib['name']] = AliasCmd(None,
+                                                  aliascmd.attrib['name'],
+                                                  aliascmd.attrib['command'])
 
     @botfreetxt(priority=1, regex=freetextRegex)
     def handle_alias(self, text, msg, command_found, freetext_found, match):
         """ Botfreetext handler that match global or user defined
-            aliases. The aliased command replaces the msg['body']
-            which is then routed again to self.bot.handle_msg_botcmd(msg).
+            aliases. The aliased command replaces the msg['body'] which
+            is then routed again to self.bot.handle_msg_botcmd(msg).
         """
-
         if command_found is True:
             return
         if msg['type'] == 'groupchat':
@@ -135,53 +151,84 @@ class alias(BotPlugin):
         if command.startswith(prefix):
             if len(prefix):
                 command = command.split(prefix, 1)[-1]
-            alias = self.aliasstore.get(aliascmd(self.bot.get_real_jid(msg), command))
-            if not alias is None:
-                msg['body'] = "%s%s %s" % (prefix, alias.command, args)
+            aliascmd = self.alias_store.get(AliasCmd(self.bot.get_real_jid(msg),
+                    command))
+            if not aliascmd is None:
+                msg['body'] = str("%s%s %s" % (prefix, aliascmd.command, args))
                 self.bot.handle_msg_botcmd(msg)
-            elif command in self.global_aliases.has_key:
-                alias = self.global_aliases[command]
-                msg['body'] = "%s%s %s" % (prefix, alias.command, args)
+            elif command in self.global_aliases:
+                aliascmd = self.global_aliases[command]
+                msg['body'] = "%s%s %s" % (prefix, aliascmd.command, args)
                 self.bot.handle_msg_botcmd(msg)
 
-    @botcmd(usage='[list|add|delete] [alias] [command] [options]')
+    @botcmd(usage="[add | del | list] [alias] [command [options]]")
     def alias(self, command, args, msg):
-        """Replace long commands (incl. options) with short words."""
+        """ Replace long commands (incl. options) with short words.
+        """
+        try:
+            args = parse_args(args, (('action',
+                                    (str, 'add', 'del', 'list')), ))
+        except ArgError as error:
+            return error.msg
 
-        if args.count(" ") > 0:
-            (option, value) = args.split(" ", 1)
-        else:
-            (option, value) = (args, None)
-        options = ['list', 'add', 'delete']
+        args.parsed_ = False
+        return getattr(self, 'alias_' + args.action,)(command, args, msg)
 
-        if not option in options:
-            return "Please supply an option."
+    @botcmd(usage="alias command [options])", hidden=True)
+    def alias_add(self, command, args, msg):
+        """ Add an alias with the given command and options.
+        """
+        try:
+            args = parse_args(args, (('action', ''), ('alias', str),
+                    ('command', str)))
+        except ArgError as error:
+            return error.msg
 
-        if option == 'add' and not value is None:
-            if value.count(" ") < 1:
-                return "Please supply a command for the alias."
-            (alias, cmd) = value.split(' ', 1)
-            self.aliasstore.update(aliascmd(self.bot.get_real_jid(msg), alias, cmd))
+        bot_commands = dict(self.bot.im_commands.items()
+                            + self.bot.muc_commands.items())
+
+        if not args.alias in bot_commands:
+            if not args.tail_ is None:
+                command = args.command + ' ' + args.tail_
+            else:
+                command = args.command
+            self.alias_store.update(AliasCmd(self.bot.get_real_jid(msg),
+                                            args.alias, command))
             return "Done."
+        else:
+            return "%s is a bot command." % args.alias
 
-        if option == 'delete':
-            alias = self.aliasstore.get(aliascmd(self.bot.get_real_jid(msg), value))
-            if not alias is None:
-                self.aliasstore.delete(aliascmd(self.bot.get_real_jid(msg), value))
-                return "Done."
-            if value in self.global_aliases:
-                return "You can not delete a global alias."
-            return "Unkown alias."
+    @botcmd(usage="alias", hidden=True)
+    def alias_del(self, command, args, msg):
+        """ Delete an alias.
+        """
+        try:
+            args = parse_args(args, (('action', ''), ('alias', str)))
+        except ArgError as error:
+            return error.msg
 
-        if option == 'list':
-            aliases = self.aliasstore.get_all(self.bot.get_real_jid(msg))
-            response = 'Aliases: '
-            if not aliases is None:
-                for alias in aliases:
-                    response += "\n%s = %s" % (alias.alias, alias.command)
-            if not self.global_aliases == {}:
-                for key, alias in self.global_aliases.iteritems():
-                    response += "\n(*) %s = %s" % (alias.alias, alias.command)
-            if response == 'Aliases: ':
-                response += "None."
-            return response
+        aliascmd = self.alias_store.get(AliasCmd(self.bot.get_real_jid(msg),
+                                    args.alias))
+        if not aliascmd is None:
+            self.alias_store.delete(AliasCmd(self.bot.get_real_jid(msg),
+                                    args.alias))
+            return "Done."
+        if args.alias in self.global_aliases:
+            return "You can not delete a global alias."
+        return "Unkown alias."
+
+    @botcmd(hidden=True)
+    def alias_list(self, command, args, msg):
+        """ List available aliases.
+        """
+        aliases = self.alias_store.get_all(self.bot.get_real_jid(msg))
+        response = 'Aliases: '
+        if not aliases is None:
+            for aliascmd in aliases:
+                response += "\n%s = %s" % (aliascmd.alias, aliascmd.command)
+        if not self.global_aliases == {}:
+            for (key, aliascmd) in self.global_aliases.iteritems():
+                response += "\n(*) %s = %s" % (aliascmd.alias, aliascmd.command)
+        if response == 'Aliases: ':
+            response += "None."
+        return response
