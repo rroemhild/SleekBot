cbc175f28d594826f5d04ad1d64128316724c62a
Rafael Römhild <rafael@roemhild.de>
2010-11-29 10:39:11 +0100

    Finished work on ACL LDAP class.

--- sleekbot/acl.py
+++ sleekbot/acl.py
@@ -6,12 +6,15 @@
 import logging
 from collections import defaultdict
 
+
+LDAP = False
 try:
     import ldap
-    import ldap.filter
+    from ldap.filter import escape_filter_chars
+    LDAP = True
 except:
-    logging.error("can not import ldap. LDAP storage for acl" \
-                    " is not available.")
+    logging.error("ldap not present. LDAP ACL storage " \
+                    "not available.")
 
 
 def parts_of(address):
@@ -59,11 +62,11 @@ class ESet(set):
         return False
 
 
-class LdapEntry(dict):
+class LDAPEntry(dict):
     """ Dict for an LDAP entry."""
-    def __init__(self, entrie):
-        for key in entrie.keys():
-            self[key] = entrie[key][0].decode('utf-8')
+    def __init__(self, entry):
+        for key in entry.keys():
+            self[key] = entry[key][0].decode('utf-8')
 
 
 class VirtualSet(object):
@@ -253,64 +256,146 @@ class ACLdb(ACL):
 
 
 class ACLldap(ACL):
-    """ LDAP storage for access control lists
+    """ LDAP storage for access control lists.
     """
 
-    def __init__(self, caller, config=None):
-        self._post_init()
+    def __init__(self, caller, config=None):       
         self.config = config
-        self.caller = caller
-        self.searchScope = ldap.SCOPE_SUBTREE
-        self.ldap = self._bind()
+        self.member_attrib = self.config.find('groups').get('memberattr', '*')
+        self.object_class = self.config.find('groups').get('groupclass', '*')
+        self.basedn = self.config.find('groups').get('basedn')
+        self._post_init()
 
-    def _bind(self):
-        """Connect to the LDAP Server"""
+    def _ldap_connect(self):
+        """ Connect to the LDAP Server.
+        """
+        if not LDAP:
+            return False
 
-        l = ldap.initialize(self.config.find('server').get('uri'))
-        l.timeout = 10
+        conn = ldap.initialize(self.config.find('server').get('uri'))
+        conn.timeout = 10
 
         try:
-            l.simple_bind_s(self.config.find('server').get('binddn'),
+            logging.debug('Bind to LDAP Server: %s.',
+                            self.config.find('server').get('uri'))
+            conn.simple_bind_s(self.config.find('server').get('binddn'),
                             self.config.find('server').get('secret'))
         except ldap.INVALID_CREDENTIALS:
             logging.error("LDAP username or password is incorrect.")
             return False
         except ldap.SERVER_DOWN:
-            logging.error("LDAP server %s down" % self.uri)
+            logging.error("LDAP server %s down.",
+                        self.config.find('server').get('uri'))
             return False
-        except ldap.LDAPError, e:
-            logging.error('LDAP %s' % e)
+        except ldap.LDAPError, error:
+            logging.error('LDAP error: %s.', error)
             return False
+        
+        return conn
 
-        logging.info('Bind to LDAP Server %s' \
-            % self.config.find('server').get('uri'))
-        return l
+    def ldap_equal(self, left, right):
+        subs = []
+        if isinstance(right, (list, tuple)):
+            for r in right:
+                subs.append("(%s=%s)" % (left, escape_filter_chars(r)))
+        else:
+            subs = "(%s=%s)" % (left, escape_filter_chars(right))
+        
+        return ''.join(subs)
 
+    def add(self, i, j):
+        return i+j
+
+    def role_entry(self, entry):
+        entry = LDAPEntry(entry[1])
+        return entry['cn']
+    
+    def ldap_search(self, sfilter, attrlist, sscope=ldap.SCOPE_SUBTREE):
+        """ Perform a search on an LDAP server.
+        """
+        conn = self._ldap_connect()
+        logging.debug("Search on LDAP Server with filter: %s.", sfilter)
+        
+        result = []
+        if conn:
+            try:
+                result = conn.search_st(self.basedn, sscope, sfilter, attrlist,
+                        0, int(self.config.find('server').get('timeout', 10)))         
+                logging.debug("Unbind from LDAP server.")
+                conn.unbind_s()
+            except ldap.FILTER_ERROR:
+                logging.error("LDAP search filter error.")
+            except ldap.LDAPError, error:
+                logging.error('LDAP error: %s.', error)
+        
+        return result
+    
     def __getitem__(self, jid):
-        if self.ldap:
-            searchFilter = "(&(objectClass=%s)(memberUid=%s))" \
-                % (self.config.find('groups').get('groupClass', '*'),
-                jid)
-            retrieveAttrib = ['cn']
-            result_set = self.ldap.search_st(
-                    self.config.find('groups').get('basedn'),
-                    self.searchScope, searchFilter, retrieveAttrib, 0,
-                    int(self.config.find('server').get('timeout', 10)))
-            entry = LdapEntry(result_set[0][1])
-            return getattr(self.caller.acl.ROLE, entry['cn'])
-
+        sfilter = "(&(objectClass=%s)(memberUid=%s))" % \
+                            (self.object_class, escape_filter_chars(jid))
+        attrlist = ['cn']
+        result = self.ldap_search(sfilter, attrlist)
+        if len(result) > 0:
+            entry = LDAPEntry(result[0][1])
+            return getattr(ACL.ROLE, entry['cn'])
+    
     def __contains__(self, jid):
-        if self.ldap:
-            searchFilter = "(&(objectClass=%s)(%s=%s))" \
-                % (self.config.find('groups').get('groupclass', '*'),
-                    self.config.find('groups').get('memberattr', '*'),
-                    jid)
-            retrieveAttrib = ['cn']
-            result_set = self.ldap.search_st(
-                    self.config.find('groups').get('basedn'),
-                    self.searchScope, searchFilter, retrieveAttrib, 0,
-                    int(self.config.find('server').get('timeout', 10)))
-            return len(result_set) > 0
+        sfilter = "(&(objectClass=%s)(%s=%s))" % \
+            (self.object_class, self.member_attrib, escape_filter_chars(jid))
+        attrlist = ['cn']
+        result = self.ldap_search(sfilter, attrlist)
+        return len(result) > 0
+
+    def check(self, jid, role):
+        """ Check if jid is in a role/roles
+                jid  -- a string with the jid or domain to check
+                role -- an item of ROLE enum or collection of such items
+        """
+        jid = tuple(parts_of(escape_filter_chars(jid)))
+        if isinstance(role, (list, tuple)):
+            query = "(|%s)(|%s)" % \
+                (self.ldap_equal(self.member_attrib, jid),
+                                    self.ldap_equal('cn', ACL.ROLE[role]))
+        else:
+            query = "(|%s)(cn=%s)" % \
+                (self.ldap_equal(self.member_attrib, jid), ACL.ROLE[role])
+
+        sfilter = "(&(objectClass=%s)%s)" % (self.object_class, query)
+        attrlist = []
+        result = self.ldap_search(sfilter, attrlist)
+        return len(result) > 0
+
+    def count(self, role=None):
+        """ Returns the number of jids that are in role.
+        """
+        if role is None:
+            query = "(%s=*)" % self.member_attrib
+            role = ()
+        elif isinstance(role, (list, tuple)):
+            query = "(%s)" % self.ldap_equal('cn', ACL.ROLE[role])
+        else:
+            query = "(cn=%s)" % ACL.ROLE[role]
+            role = (role, )
+        
+        sfilter = "(&(objectClass=%s)%s)" % (self.object_class, query)
+        attrlist = ['cn']
+        result = self.ldap_search(sfilter, attrlist)
+        
+        sfilter = '(|%s)' % self.ldap_equal('cn', map(self.role_entry,
+                                                        result))
+        attrlist = [self.member_attrib]
+        result = self.ldap_search(sfilter, attrlist)
+        return reduce(self.add, map(len, result), 0)
+        
+    def __setitem__(self, jid, role):
+        """ No write support """
+        logging.error("No LDAP write support.")
+        return False
+
+    def __delitem__(self, jid):
+        """ No write support """
+        logging.error("No LDAP write support.")
+        return False
 
 
 if __name__ == '__main__':
@@ -323,3 +408,4 @@ if __name__ == '__main__':
         print("---> ", s)
         for x in xs:
             print('%s: %s' % (x, x in s))
+
